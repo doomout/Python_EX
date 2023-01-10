8a797eb47e58c453d56afa43ca1ab3d3ae7272ff
doomout <winquake3@hanmail.net>
2023-01-10 20:19:33 +0900

    시간 표시 추가

---
+++ Date1.py
@@ -0,0 +1,11 @@
+import datetime
+
+today = datetime.datetime.now()
+
+print("지금은", today, "입니다.")
+print("지금은", today.year, "년 입니다.")
+print("지금은", today.month, "월 입니다.")
+print("지금은", today.day, "일입니다.")
+print("지금은", today.hour, "시입니다.")
+print("지금은", today.minute, "분입니다.")
+print("지금은", today.second, "초입니다.")
