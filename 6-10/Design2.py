987392463b5d5ea1d2a35e45220661897c0160d3
doomout <winquake3@hanmail.net>
2023-01-13 16:39:28 +0900

    테스트 코드 삭제

--- 6-10/Design2.py
+++ 6-10/Design2.py
@@ -1,20 +1,16 @@
 gameWord = "apple"
-guessedLetters = ['a', 'e']
+guessedLetters = []
 maskChar = "_"
 
 #빈 문자열로 시작하기
 displayWord = ""
 #한 문자씩 반복하기
 for letter in gameWord:
-    print(letter)
     #추측한 문자가 맞나?
     if letter in guessedLetters:
-        print("맞앗쓰")
         displayWord += letter
     else:
-        print("틀렸쓰")
         displayWord += maskChar
-    print("displayWord는", displayWord, "입니다.")
 #결과 단어 출력하기
 print("원래 단어: ", gameWord)
 print("마스킹한 단어: ", displayWord)
