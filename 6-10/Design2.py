57f4dea625ac1d06f7c1947b6c538e2931b29e50
doomout <winquake3@hanmail.net>
2023-01-13 15:53:32 +0900

    사용자 추측 문자 저장

---
+++ 6-10/Design2.py
@@ -0,0 +1,14 @@
+#추측 문자를 저장할 리스트
+guessedLetters = []
+
+for i in range(0,5):
+    #추측 입력받기
+    currGuess = input("추측 문자: ").strip().lower()
+    #리스트 추가하기
+    guessedLetters.append(currGuess)
+
+#리스트 정렬하기
+guessedLetters.sort()
+
+#출력하기
+print(guessedLetters)
