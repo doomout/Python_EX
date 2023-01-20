08832021ea26b492c07969aa944b383192827753
doomout <winquake3@hanmail.net>
2023-01-20 18:06:17 +0900

    게임 루프 추가

--- CrazyDriver/Main.py
+++ CrazyDriver/Main.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+from pygame.locals import * #pygame 라이브러리의 모든 지역 변수를 불러오기 위해
 
 #게임 색상 정의 하기(상수는 대문자로 지정)
 BLACK = (0,0,0)
@@ -24,5 +26,14 @@ screen = pygame.display.set_mode((500,800))
 #배경 색상 결정하기
 screen.fill(WHITE)
 
-#화면 업데이트 하기
-pygame.display.update()
+#메인 게임 루프(시작~끝까지 이 안에서 동작한다)
+while True:
+    #이벤트 확인하기
+    for event in pygame.event.get():
+        #플레이어가 게임을 그만 두는가?
+        if event.type == QUIT:
+            #게임 끝내기
+            pygame.quit()
+            sys.exit()
+    #화면 업데이트 하기
+    pygame.display.update()
